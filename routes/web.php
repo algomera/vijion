030cae9677bf7cf48a2744dc5ecdfb7bc830c014
fabioserembe <fabio.serembe@gmail.com>
2023-03-24 12:46:17 +0100

    - Creazione nuovo layout (Auth) utilizzato per le pagine di autenticazione (login, registrazione, password dimenticata ecc..) sostituendo il vecchio Guest; - Utilizzato il layout Guest per le pagine "comuni" (homepage ecc..); - Creazione UserSeeder che inserisce un utente "admin" (nessun ruolo ancora impostato) e altri 9 utenti "normali"; - Adattamento delle routes di conseguenza;

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,36 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+	use App\Http\Controllers\ProfileController;
+	use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+	// Guest
+	Route::middleware('guest')->group(function () {
+		Route::get('/', function () {
+			return view('home');
+		});
+	});
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+	// Auth
+	Route::middleware('auth')->group(function () {
+		// Dashboard
+		Route::get('/dashboard', function () {
+			return view('dashboard');
+		})->middleware([
+			'auth',
+			'verified'
+		])->name('dashboard');
+		// Profile
+		Route::get('/profile', [
+			ProfileController::class,
+			'edit'
+		])->name('profile.edit');
+		Route::patch('/profile', [
+			ProfileController::class,
+			'update'
+		])->name('profile.update');
+		Route::delete('/profile', [
+			ProfileController::class,
+			'destroy'
+		])->name('profile.destroy');
+	});
+	require __DIR__ . '/auth.php';
