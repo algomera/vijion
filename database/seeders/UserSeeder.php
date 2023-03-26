4651921e4e52191c1c6b455753f0006813f51ce9
fabioserembe <fabio.serembe@gmail.com>
2023-03-26 17:27:40 +0200

    - Aggiunto pacchetto laravel-permission; - Creato ruolo "admin" e assegnato al primo utente; - Fix vecchi "if role" con la nuova direttiva @role; - Modifica migrazione users aggiungendo colonna coins;

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@
 	use App\Models\User;
 	use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 	use Illuminate\Database\Seeder;
+	use Spatie\Permission\Models\Role;
 
 	class UserSeeder extends Seeder
 	{
@@ -12,10 +13,13 @@
 		 * Run the database seeds.
 		 */
 		public function run(): void {
+			$adminRole = Role::create(['name' => 'admin']);
 			$admin = User::factory()->create([
 				'name'  => 'Admin',
 				'email' => 'admin@example.test',
+				'coins' => null
 			]);
+			$admin->assignRole($adminRole);
 			User::factory(9)->create();
 		}
 	}
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -176,39 +176,40 @@
 		<div class="hidden lg:flex lg:items-center lg:space-x-3">
 			@auth
 				<div class="flex items-center space-x-3 border border-gray-200 rounded-full p-1">
-					@if(auth()->id() !== 1)
-						<div class="bg-brand rounded-full text-sm text-white py-2 px-4">
-							<p class="font-bold">
-								60 <span class="hidden xl:inline-block text-xs font-semibold">VIJI-COINS</span>
-							</p>
-						</div>
-					@endif
+					@unlessrole('admin')
+					<div class="bg-brand rounded-full text-sm text-white py-2 px-4">
+						<p class="font-bold">
+							{{ auth()->user()->coins }} <span class="hidden xl:inline-block text-xs font-semibold">VIJI-COINS</span>
+						</p>
+					</div>
+					@endunlessrole
 					<div class="flex items-center space-x-2 ml-1 text-sm text-gray-500">
 						<p class="font-semibold">Ciao {{ auth()->user()->name }}</p>
 						<x-dropdown align="right" width="48">
 							<x-slot name="trigger">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
 								     stroke-width="1.5"
-								     stroke="currentColor" class="w-7 h-7 hover:text-brand cursor-pointer" :class="{'text-brand': open}">
+								     stroke="currentColor" class="w-7 h-7 hover:text-brand cursor-pointer"
+								     :class="{'text-brand': open}">
 									<path stroke-linecap="round" stroke-linejoin="round"
 									      d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"/>
 								</svg>
 							</x-slot>
 
 							<x-slot name="content">
-								@if(auth()->id() === 1)
-									<x-dropdown-link :href="route('dashboard')">
-										Dashboard
-									</x-dropdown-link>
-								@endif
+								@role('admin')
+								<x-dropdown-link :href="route('dashboard')">
+									Dashboard
+								</x-dropdown-link>
+								@endrole
 								<x-dropdown-link :href="route('profile.edit')">
 									Profilo
 								</x-dropdown-link>
 
 								<!-- Authentication -->
-									<x-dropdown-link wire:click="logout">
-										{{ __('Log Out') }}
-									</x-dropdown-link>
+								<x-dropdown-link wire:click="logout">
+									{{ __('Log Out') }}
+								</x-dropdown-link>
 							</x-slot>
 						</x-dropdown>
 					</div>
