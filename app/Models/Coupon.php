98e0e9331289fbae7b601686aca0c415a73e8d7b
fabioserembe <fabio.serembe@gmail.com>
2023-03-29 11:50:38 +0200

    - Aggiunto scope Available a Coupon per restituire i coupon disponibili (non scaduti);

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,6 +2,7 @@
 
 	namespace App\Models;
 
+	use Illuminate\Database\Eloquent\Builder;
 	use Illuminate\Database\Eloquent\Factories\HasFactory;
 	use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,9 @@
 	{
 		use HasFactory;
 
+		public function scopeAvailable(Builder $query) {
+			$query->whereDate('expires_date', '>', now())->orWhere('expires_date', null);
+		}
 		public function brand() {
 			return $this->belongsTo(Brand::class);
 		}
