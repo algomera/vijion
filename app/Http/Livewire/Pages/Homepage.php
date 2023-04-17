ac5407ec7e7900da459b4fc80cf86140c0469aef
fabioserembe <fabio.serembe@gmail.com>
2023-04-17 17:04:10 +0200

    - Rimosso limite (take()) da "Gli imperdibili" e da "Offerte della settimana"; - Rimosso pulsante "Visualizza tutti" da "Offerte della settimana";

--- app/Http/Livewire/Pages/Homepage.php
+++ app/Http/Livewire/Pages/Homepage.php
@@ -34,8 +34,8 @@
 			return view('livewire.pages.homepage', [
 				'slides'           => HeroSlide::where('visible', 1)->get()->sortBy('order'),
 				'our_brands'       => Brand::whereHas('coupons')->where('our_brand', 1)->orderBy('our_brand_order')->get()->take(8),
-				'unmissables'      => Brand::whereHas('coupons')->where('unmissable', 1)->orderBy('unmissable_order')->get()->take(8),
-				'week_offers'      => Brand::whereHas('coupons')->where('week_offer', 1)->orderBy('week_offer_order')->get()->take(8),
+				'unmissables'      => Brand::whereHas('coupons')->where('unmissable', 1)->orderBy('unmissable_order')->get(),
+				'week_offers'      => Brand::whereHas('coupons')->where('week_offer', 1)->orderBy('week_offer_order')->get(),
 				'category_1'       => \App\Models\Category::where('highlighted_spot', 1)->first(),
 				'category_2'       => \App\Models\Category::where('highlighted_spot', 2)->first(),
 				'category_3'       => \App\Models\Category::where('highlighted_spot', 3)->first(),
