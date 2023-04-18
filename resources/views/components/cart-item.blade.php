58eb19c27411d5d8caee92aee9b5106f473fe528
fabioserembe <fabio.serembe@gmail.com>
2023-04-18 17:23:32 +0200

    - Fix grafici e miglioramenti al responsive per la pagina Carrello;

--- resources/views/components/cart-item.blade.php
+++ resources/views/components/cart-item.blade.php
@@ -1,7 +1,8 @@
 @props(['coupon'])
 <div x-data="" class="flex items-center space-x-5 py-5">
-	<x-heroicon-o-x-mark x-on:click="Livewire.emit('removeFromCart', {{ $coupon->id }})" class="w-6 h-6 text-gray-300 cursor-pointer flex-shrink-0 hover:text-gray-500" />
-	<div class="relative w-32 aspect-square">
+	<x-heroicon-o-x-mark x-on:click="Livewire.emit('removeFromCart', {{ $coupon->id }})"
+	                     class="w-6 h-6 text-gray-300 cursor-pointer flex-shrink-0 hover:text-gray-500"/>
+	<div class="hidden relative w-32 aspect-square md:block">
 		@if(!$coupon->bg)
 			<img src="{{ $coupon->brand->category->image_path }}"
 			     alt="{{ $coupon->brand->name }}" class="aspect-square w-full object-cover">
@@ -15,7 +16,24 @@
 			<h5 class="text-gray-900 font-semibold">{{ $coupon->brand->category->name }}</h5>
 			<span class="text-brand font-semibold text-sm">{{ $coupon->brand->name }}</span>
 		</div>
-		<div class="flex">
+		<div class="block md:hidden">
+			<div class="flex items-center space-x-2">
+				<span class="text-gray-900 font-semibold text-sm">{{ $coupon->amount }}{{ $coupon->type === 'percentage' ? '%' : '€' }}</span>
+				<span class="text-gray-400">&middot;</span>
+				<div class="flex items-center space-x-1 bg-white text-brand">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+					     stroke-width="1.5"
+					     stroke="currentColor" class="w-5 h-5 flex-shrink-0">
+						<path stroke-linecap="round" stroke-linejoin="round"
+						      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+						<path stroke-linecap="round" stroke-linejoin="round"
+						      d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"/>
+					</svg>
+					<span class="font-black">{{ $coupon->coins }}</span>
+				</div>
+			</div>
+		</div>
+		<div class="hidden md:flex">
 			<div class="flex flex-col justify-center text-center bg-gray-50 border border-gray-100 h-16 px-8 py-1.5">
 				<span class="leading-none text-2xl font-bold">{{ $coupon->amount }}{{ $coupon->type === 'percentage' ? '%' : '€' }}</span>
 				<span class="text-xs text-gray-500">(sconto)</span>
