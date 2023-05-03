d1faf0e4d521fec43ae611ca62321c127cd023b4
fabioserembe <fabio.serembe@gmail.com>
2023-05-03 12:12:19 +0200

    - Aggiunta colonna "teyuto_id" alla tabella users; - Creazione endpoint per registrazione utente;

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,49 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+	use App\Models\User;
+	use Illuminate\Http\Request;
+	use Illuminate\Support\Facades\Route;
+	use Illuminate\Support\Facades\Validator;
+	use Spatie\Permission\Models\Role;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| API Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register API routes for your application. These
+	| routes are loaded by the RouteServiceProvider and all of them will
+	| be assigned to the "api" middleware group. Make something great!
+	|
+	*/
+
+	Route::middleware('api')->group(function() {
+		Route::post('/register', function(Request $request) {
+			$rules = [
+				'first_name' => 'required',
+				'last_name'  => 'required',
+				'email'      => 'required|email|unique:users,email',
+				'password'   => 'required|confirmed',
+			];
+
+			$validator = Validator::make($request->all(), $rules);
+
+			if($validator->fails()) {
+				return $validator->messages();
+			} else {
+				$user = User::create([
+					'first_name' => $request->get('first_name'),
+					'last_name'  => $request->get('last_name'),
+					'email'      => $request->get('email'),
+					'password'   => md5($request->get('password')),
+					'coins'      => 0,
+				]);
+				$user->assignRole(Role::findByName('member'));
+				return "User Registered";
+			}
+		});
+	});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
