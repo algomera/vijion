ef00e4f8cea3983a8a4279792f7d3735f13b261e
fabioserembe <fabio.serembe@gmail.com>
2023-05-10 10:06:56 +0200

    - La registrazione di un utente tramite API avviene secondo il json che arriva dal webhook;

--- routes/api.php
+++ routes/api.php
@@ -16,26 +16,42 @@
 	| be assigned to the "api" middleware group. Make something great!
 	|
 	*/
-
-	Route::middleware('api')->group(function() {
-		Route::post('/register', function(Request $request) {
+	Route::middleware('api')->group(function () {
+		/*
+		{
+			"event": "user_created",
+			"object": {
+				"id": "0123456789",
+				"username": "test-username",
+				"email": "test@email.com",
+				"date": "2019-05-18T07:59:12+02:00",
+				"language": "en",
+				"location": {
+					"city": "city",
+					"country": "country",
+					"country_flag": "flag",
+					"country_code": "code"
+				}
+			},
+			"datetime": "2023-05-04T08:49:49+02:00"
+		}
+		 */
+		Route::post('/register', function (Request $request) {
 			$rules = [
-				'first_name' => 'required',
-				'last_name'  => 'required',
-				'email'      => 'required|email|unique:users,email',
-				'password'   => 'required|confirmed',
+				'id'       => 'required',
+				'username' => 'required',
+				'email'    => 'required|email|unique:users,email'
 			];
-
-			$validator = Validator::make($request->all(), $rules);
-
-			if($validator->fails()) {
+			$validator = Validator::make($request->get('object'), $rules);
+			if ($validator->fails()) {
 				return $validator->messages();
 			} else {
 				$user = User::create([
-					'first_name' => $request->get('first_name'),
-					'last_name'  => $request->get('last_name'),
-					'email'      => $request->get('email'),
-					'password'   => md5($request->get('password')),
+					'teyuto_id'  => $request->get('object')['id'],
+					'first_name' => $request->get('object')['username'],
+					'last_name'  => null,
+					'email'      => $request->get('object')['email'],
+					'password'   => md5(\Illuminate\Support\Str::password(10, true, true, false)),
 					'coins'      => 0,
 				]);
 				$user->assignRole(Role::findByName('member'));
@@ -43,7 +59,6 @@
 			}
 		});
 	});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-	return $request->user();
-});
+	Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+		return $request->user();
+	});
