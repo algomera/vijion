f830dca3219a639b8a4ddffa2227bb698f88c3b6
fabioserembe <fabio.serembe@gmail.com>
2023-05-25 11:04:07 +0200

    - DatabaseSeeder.php ora mette in produzione i ruoli;

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,20 +15,20 @@
 		public function run(): void {
 			$adminRole = Role::create(['name' => 'admin']);
 			$memberRole = Role::create(['name' => 'member']);
-			$admin = User::factory()->create([
-				'first_name'  => 'Admin',
-				'last_name' => null,
-				'email' => 'admin@example.test',
-				'coins' => null
-			]);
-			$admin->assignRole($adminRole);
-			$member = User::factory()->create([
-				'first_name' => 'Algomera',
-				'last_name'  => null,
-				'email'      => 'info@algomera.it'
-			]);
-			$member->assignRole($memberRole);
             if(app()->environment() === "local") {
+                $admin = User::factory()->create([
+                    'first_name'  => 'Admin',
+                    'last_name' => null,
+                    'email' => 'admin@example.test',
+                    'coins' => null
+                ]);
+                $admin->assignRole($adminRole);
+                $member = User::factory()->create([
+                    'first_name' => 'Algomera',
+                    'last_name'  => null,
+                    'email'      => 'info@algomera.it'
+                ]);
+                $member->assignRole($memberRole);
                 User::factory(8)->create()->each(function ($u) use ($memberRole) {
                     $u->assignRole($memberRole);
                 });
