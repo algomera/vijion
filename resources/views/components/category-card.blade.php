da5b30e88d4ad447feaa8053cb07707d10eb3fc5
fabioserembe <fabio.serembe@gmail.com>
2023-05-25 15:23:06 +0200

    - url() per le varie immagini;

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,7 +1,7 @@
 @props(['category'])
 
 <div class="group relative aspect-video col-span-2 sm:aspect-none sm:relative sm:h-full md:col-span-1">
-	<img src="{{ asset($category->image_path) }}" alt="" class="object-cover object-center absolute inset-0 h-full w-full">
+	<img src="{{ url($category->image_path) }}" alt="" class="object-cover object-center absolute inset-0 h-full w-full">
 	<div aria-hidden="true" class="bg-gradient-to-b from-transparent to-black opacity-5 sm:absolute sm:inset-0"></div>
 	<div class="relative flex w-full h-full flex-col items-start justify-start bg-black bg-opacity-40 p-9">
 		<p class="mt-1 text-xl lg:text-3xl font-medium text-white">Tutto su {{ $category->name }}</p>
